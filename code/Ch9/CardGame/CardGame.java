5617358a457be01dfa666cebea878255cbff3427
cse2016hy <highest416@naver.com>
2022-11-28 11:08:38 +0900

    minor

--- code/Ch9/CardGame/CardGame.java
+++ code/Ch9/CardGame/CardGame.java
@@ -5,14 +5,21 @@ public class CardGame {
 		CardPlayer p1 = new HumanPlayer(20);
 		CardPlayer p2 = new ComputerPlayer(20);
 		CardDeck cd = new CardDeck(); 
-		while (p1.wantsACard() || p2.wantsACard()) {
+		boolean p1_wants_card = true; 
+		boolean p2_wants_card = true;
+		while (p1_wants_card || p2_wants_card) {
 			Card c; 
-			c = cd.newCard(); 
-			p1.receiveCard(c);
-			System.out.println("Player 1 gets " + c.suitOf() + ":" + c.countOf());
-			c = cd.newCard();
-			p2.receiveCard(c);
-			System.out.println("Player 2 gets " + c.suitOf() +  ":" + c.countOf());
+			if (p1_wants_card && (p1_wants_card = p1.wantsACard())) {
+				c = cd.newCard(); 
+				p1.receiveCard(c);
+				System.out.println("Player 1 gets " + c.suitOf() + ":" + c.countOf());
+			}
+			if (p2_wants_card && (p2_wants_card = p2.wantsACard())) {
+				p2_wants_card = p2.wantsACard();
+				c = cd.newCard();
+				p2.receiveCard(c);
+				System.out.println("Player 2 gets " + c.suitOf() +  ":" + c.countOf());
+			}
 		}
 		Card[] p1_cards = p1.showCards();
 		Card[] p2_cards = p2.showCards();
